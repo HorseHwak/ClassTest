d3bd3f51cff4b6981a3c183713bf510285e9ddbd
HorseHwak <rhkralstn91@naver.com>
2019-06-18 15:33:29 +0900

    Quiz 1

--- ClassTest/PracticeCalss.cs
+++ ClassTest/PracticeCalss.cs
@@ -433,6 +433,10 @@ namespace ClassTest
             // 둘레길이: 9
         }
 
+        public static void practice13_2()
+        {
+
+        }
         class Triangle
         {
             private int A, B, C;
@@ -452,43 +456,6 @@ namespace ClassTest
 
         }
 
-        public static void Quiz1()
-        {
-            int sum = 0;
-
-            for (int i = 1; i < 1000; i++)
-            {
-                if (i % 3 == 0 || i % 5 == 0)
-                     sum += i; 
-            }
-            Console.WriteLine("1~1000사이 3과5의 배수의 합은 : {0} 입니다", sum);
-        }
-        public static void Quiz2()
-        {
-            int[] fibo = new int[33];
-            int sum = 0;
-
-            //피보나치수열 
-            for (int i = 0; i < fibo.Length; i++)
-            {
-                if(i == 0 || i == 1)
-                fibo[i] = 1;
        
-                else
-                fibo[i] = fibo[i-1] + fibo[i-2];
-            }
-
-            //짝수판별
-            for (int i = 0; i < fibo.Length; i++)
-            {
-                if (fibo[i] % 2 == 0)
-                    sum += fibo[i];
-            }
-          
-            Console.WriteLine("피보나치 수열 : {0}", string.Join<int>(",", fibo));
-            Console.WriteLine("짝수의 합 : {0}", sum);
-        }
-
-        
     }
  }
---
+++ ClassTest/QuizClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTest
+{
+    public static class QuizClass
+    {
+        public static void Quiz1()
+        {
+            int sum = 0;
+
+            for (int i = 1; i < 1000; i++)
+            {
+                if ((i % 3 == 0) || (i % 5 == 0))
+                    sum += i;
+            }
+            Console.WriteLine("1~1000사이 3과5의 배수의 합은 : {0} 입니다", sum);
+        }
+        
+    }
+}
